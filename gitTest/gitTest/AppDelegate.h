fe72ad3569a5249485079e962739dd312c1a8449
shengkl <810959144@qq.com>
2017-04-03 16:35:16 +0800

    创建一个新工程
    用来测试

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by mac2015 on 17/4/3.
+//  Copyright © 2017年 magickey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ gitTest/gitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitTest
+//
+//  Created by mac2015 on 17/4/3.
+//  Copyright © 2017年 magickey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
